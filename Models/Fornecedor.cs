02c56f776646a054e787a922e8a584736c988ba7
João Paulo Ghissardi da Silva <jp17paulo@terra.com.br>
2022-02-11 15:04:17 -0300

    Adicionado JsonIgnore para o Cpf_Cnpj

--- Models/Fornecedor.cs
+++ Models/Fornecedor.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Data.SqlClient;
+using System.Text.Json.Serialization;
 
 namespace api_desafio21dias.Models
 {
@@ -25,6 +26,7 @@ namespace api_desafio21dias.Models
 
     [Column("cpf_cnpj")]
     [Required]
+    [JsonIgnore]
     public string Cpf_Cnpj { get; set; }
 
     [NotMapped]
